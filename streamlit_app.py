ffd7b5156fbbddd8e95ccd93ca31ca7e94e24b31
EdmonEduardo <44651444+EdmonEduardo@users.noreply.github.com>
2025-11-13 04:31:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition information')
 
         search_on = session.table("smoothies.public.fruit_options").filter(col("FRUIT_NAME") == fruit_chosen).select(col("SEARCH_ON"))
+        st.write('xxxx : ' + search_on)
         
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
